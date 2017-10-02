e5eba087e006c3d1869a29cd9e44e032e6c0f6f1
roopkumard19 <roopkumar_d19@hotmail.com>
2017-10-01 19:59:50 -0700

    1st update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 import flask
 from flask_restful import Resource, Api
-
+from twilio.twiml.voice_response import VoiceResponse
 
 app = flask.Flask(__name__)
 api = Api(app)
@@ -9,18 +9,28 @@ api = Api(app)
 @app.route('/')
 
 def initiate_call():
-	account_sid = "AC233559065bfe688e5119167aa22b7e68"
-	auth_token = "9aaea5a1e242c18d67a6fb045311bf4f"
-	client = Client(account_sid, auth_token)
+        account_sid = "AC1a9488ee68428a8b4ce01ab2ce376b02"
+        auth_token = "6feeef65f6c56367fe7b5817dd14da17"
+        client = Client(account_sid, auth_token)
 
-	call = client.calls\
-      		.create(to="+16692547860",  # Any phone number
-              		from_="+15005550006")# a valid Twilio number)
-              		#url="http://demo.twilio.com/docs/voice.xml"
-			
+        call = client.calls\
+                .create(to="+16692547860",  # Any phone number
+                        from_="+13152281086", # a valid Twilio number
+                        url="http://demo.twilio.com/docs/voice.xml")
 
-	return str(call.sid)
 
+        return "call connected"
+
+@app.route('/disconnect')
+
+def disconnect_call():
+        twiml_response = VoiceResponse()
+        twiml_response.say('Hello!')
+        twiml_response.hangup()
+	
+	return "call disconnected"	
+
+	
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
